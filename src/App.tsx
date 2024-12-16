8ba19b48ff7917d1a6e37bcbec8586f4e10b4cb0
BesoBerdzenishvili <76405148+BesoBerdzenishvili@users.noreply.github.com>
2024-12-16 18:09:56 +0400

    add: logo (#23)

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,23 @@ import Login from "./auth/Login";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          {/* add translate to auth pages */}
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          {/* redirect user to 404 if status !== admin */}
-          {/* or as temporary measure restrict visibility of admin page if currentUser.status !== admin */}
-          <Route path="/admin-panel" element={<AdminPanel />} />
-          {/* move up aggregation component in template folder */}
-          <Route path="/template/:id" element={<Template />} />
-          <Route path="/user-form/:id" element={<UserForm />} />
-          <Route path="/user-panel" element={<UserPanel />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* add translate to auth pages */}
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        {/* redirect user to 404 if status !== admin */}
+        {/* or as temporary measure restrict visibility of admin page if currentUser.status !== admin */}
+        <Route path="/admin-panel" element={<AdminPanel />} />
+        {/* move up aggregation component in template folder */}
+        <Route path="/template/:id" element={<Template />} />
+        <Route path="/user-form/:id" element={<UserForm />} />
+        <Route path="/user-panel" element={<UserPanel />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
