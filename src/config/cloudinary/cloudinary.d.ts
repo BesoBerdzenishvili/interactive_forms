a1472614ec54bf03223992c9cc0e444727f8beff
fliderman01 <fliderman01@gmail.com>
2024-12-27 19:03:33 +0400

    refactor: remove comments

--- src/config/cloudinary/cloudinary.d.ts
+++ src/config/cloudinary/cloudinary.d.ts
@@ -1,9 +1,7 @@
-// cloudinary.d.ts
 declare global {
   interface Window {
     cloudinary: any;
   }
 }
 
-export {}; // This line makes the file a module
-// remove comment when you understand it
+export {};
